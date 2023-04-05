ab63c0b807eac0da157e9d873884708cd3273a82
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-05 21:31:22 +0200

    palindroma check function

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -3,8 +3,12 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 
 //-----------------------PROGRAMMA PRINCIPALE----------------------------
+while(true){
 
+let userInputString = prompt("Inserisci una parola");
 
+isPalindroma(userInputString);
+}
 
 
 
@@ -15,3 +19,30 @@
 
 
 //-----------------------------FUNZIONI----------------------------------
+//defiinisco la lunghezza della parola
+
+//la prima lettera è uguale all'ultima lettera?
+
+//la seconda lettera è uguale alla penultima lettera?
+function isPalindroma(word){
+let arrayDiLettere = [];
+let arrayDiControllo = [];
+//PUSHO OGNI LETTERA DELLA PAROLA NELL'ARRAY
+for(let i =0; i<word.length; i++){
+//PUSHO NELL'ARRAY DI LETTERE IN ORDINE NORMALE
+let letter = word.slice(i, i+1);
+arrayDiLettere.push(letter);
+//PUSHO NELL'ARRAY DI CONTROLLO OGNI LETTERA IN ORDINE INVERSO
+let controlLetter = word.slice((word.length - i - 1), word.length - i);
+arrayDiControllo.push(controlLetter)
+if(arrayDiLettere[i]!=arrayDiControllo[i]){
+    alert(word + " non è palindroma!");
+    break;
+} else{ 
+    alert(word + " è palindroma");
+    break;
+};
+
+}
+}
+
